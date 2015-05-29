457c94ed9ded809b35a1c64bf25f835f8168dd9f
kid143 <primecriminalkid@gmail.com>
2015-05-29 14:36:17 +0800

    add macd. x axis must be reprogrammed for better ticking.

--- tushare_test.py
+++ tushare_test.py
@@ -8,8 +8,64 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import tushare as ts
-from matplotlib.finance import candlestick2_ochl
 from datetime import timedelta
+from matplotlib.lines import Line2D
+from matplotlib.patches import Rectangle
+
+
+def candlestick(ax, data, width=0.6, colorup='r', colordown='g', time_format="%Y-%m-%d", alpha=0.7):
+    """ Optimized candle stick graph interface."""
+    offset = width / 2.0
+    timeseries = data.index
+    t = [tt.strftime(time_format) for tt in timeseries]
+    oo = data['open']
+    cc = data['close']
+    hh = data['high']
+    ll = data['low']
+
+    ax.set_xticks([x*width for x in range(len(t))], minor=True)
+    ax.set_xlim(0, len(t)*width)
+    ax.set_xticklabels(t, rotation=90, horizontalalignment='right', minor=True)
+
+    lines = []
+    patches = []
+    for q in range(len(t)):
+        c = float(cc[q])
+        o = float(oo[q])
+        h = float(hh[q])
+        l = float(ll[q])
+        if c >= o:
+            color = colorup
+            lower = o
+            height = c - o
+        else:
+            color = colordown
+            lower = c
+            height = o - c
+
+        vline = Line2D(
+            xdata=(q*width, q*width), ydata=(l, h),
+            color=color,
+            linewidth=0.5,
+            antialiased=True,
+        )
+
+        rect = Rectangle(
+            xy=(q*width-offset, lower),
+            width = width,
+            height = height,
+            facecolor = color,
+            edgecolor = color,
+        )
+        rect.set_alpha(alpha)
+
+        lines.append(vline)
+        patches.append(rect)
+        ax.add_line(vline)
+        ax.add_patch(rect)
+
+    ax.autoscale_view()
+    return lines, patches
 
 
 class PriceData(object):
@@ -34,28 +90,9 @@ class PriceData(object):
         :rtype fig matplotlib.figure.Figure
         :rtype ax matplotlib.axes.Axes
         """
-        timeseries = self._data.index
-        open_p = self._data['open']
-        close_p = self._data['close']
-        high_p = self._data['high']
-        low_p = self._data['low']
-
         fig, ax = plt.subplots()
 
-        ax.autoscale_view()
-
-        x_len = len(timeseries)
-        rest = (x_len - 1) % 20
-        last = timeseries[-1]
-        last_time = last + timedelta(20 - rest)
-        ticklabels = [timeseries[d].strftime('%Y-%m-%d') for d in range(len(timeseries)) if d % 20 == 0]
-        if last_time != last:
-            ticklabels.append(last_time.strftime('%Y-%m-%d'))
-        ax.tick_params(axis='both', direction='out', width=2, length=8,
-                       labelsize=len(ticklabels), pad=8)
-        ax.set_xticklabels(ticklabels, rotation=45, horizontalalignment='right')
-
-        candlestick2_ochl(ax, open_p, close_p, high_p, low_p, width=0.6, colorup='r', colordown='g')
+        candlestick(ax, self._data)
 
         fig.show()
         return fig, ax
@@ -66,7 +103,7 @@ class PriceData(object):
         for para in ma_params:
             ma_s = pd.rolling_mean(close_p, para)
             # switch scalex to False to prevent breaking the x axis tick
-            ax.plot(ma_s.values.tolist(),
+            ax.plot([x*0.6 for x in range(len(close_p))], ma_s.values.tolist(),
                     color=np.random.random(3).tolist(),
                     linestyle='-',
                     label='MA'+str(para),
@@ -76,8 +113,59 @@ class PriceData(object):
         ax.legend(loc='best')
         return fig, ax
 
+    def candlestick_with_macd(self, macd_params=(12, 26, 9)):
+        """ Draw candle stick graph with macd graph"""
+        close_p = self._data['close']
+
+        left, width = 0.1, 0.8
+        fig = plt.figure(facecolor='white')
+        main_rect = [left, 0.4, width, 0.5]
+        macd_rect = [left, 0.1, width, 0.3]
+
+        axescolor = '#f6f6f6'
+
+        main_g = fig.add_axes(main_rect, axis_bgcolor=axescolor)
+        macd_g = fig.add_axes(macd_rect, axis_bgcolor=axescolor, sharex=main_g)
+
+        macd_g.autoscale_view()
+
+        candlestick(main_g, self._data, width=0.6, colorup='r', colordown='g')
+
+        # Draw macd graph
+        fast_para, slow_para, smooth_para = macd_params
+        fast = pd.ewma(close_p, fast_para)
+        slow = pd.ewma(close_p, slow_para)
+        diff = fast - slow
+        dea = pd.ewma(diff, smooth_para)
+        macd = diff - dea
+
+        macd_g.plot([x*0.6 for x in range(len(close_p))],
+                    diff.values.tolist(),
+                    color='black',
+                    linestyle='-',
+                    label='DIF',
+                    linewidth=1.0,
+                    scalex=False)
+        macd_g.plot([x*0.6 for x in range(len(close_p))],
+                    dea.values.tolist(),
+                    color='blue',
+                    linestyle='-',
+                    label='DEA',
+                    linewidth=1.0,
+                    scalex=False)
+        fillcolor = 'darkslategrey'
+        macd_g.fill_between([x*0.6 for x in range(len(close_p))],
+                            macd, 0, alpha=0.5,
+                            facecolor=fillcolor,
+                            edgecolor=fillcolor
+                            )
+
+        macd_g.legend(loc='best')
+        fig.show()
+        return fig, main_g, macd_g
+
 
 if __name__ == '__main__':
     df = ts.get_h_data('601166', start='2014-09-01', end='2015-05-28')
     price_data = PriceData(df)
-    price_data.candlestick_with_ma()
+    price_data.candlestick_with_macd()
