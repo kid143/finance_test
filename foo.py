046bcf90564de79ee4ca21d721b6a62830b515f1
kid143 <primecriminalkid@gmail.com>
2015-06-07 13:27:45 +0800

    add test for jekins CI

---
+++ foo.py
@@ -0,0 +1,6 @@
+# -*- coding:utf-8 -*-
+from __future__ import division
+
+
+def divide(n1, n2):
+	return n1/n2
---
+++ foo_test.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+from __future__ import division
+
+
+import unittest
+import foo
+
+
+class SimpleTest(unittest.TestCase):
+	def test1(self):
+		self.assertEqual(foo.divide(2, 2), 1)
+    
+    def test2(self):
+    	self.assertEqual(foo.divide(0, 1), 1)
+
+
+if __name__ == '__main__':
+	unittest.main()
